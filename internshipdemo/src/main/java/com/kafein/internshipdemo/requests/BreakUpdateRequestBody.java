bea3840fc809fbc3bd0d52a446a173f544ed1cdf
mertcantoglu <76395574+mertcantoglu@users.noreply.github.com>
2024-07-04 20:31:40 +0300

    controllers redesgined

--- internshipdemo/src/main/java/com/kafein/internshipdemo/requests/BreakUpdateRequestBody.java
+++ internshipdemo/src/main/java/com/kafein/internshipdemo/requests/BreakUpdateRequestBody.java
@@ -2,8 +2,10 @@ package com.kafein.internshipdemo.requests;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 
@@ -15,22 +17,22 @@ import java.util.Date;
 public class BreakUpdateRequestBody {
 
 
-    @NotEmpty(message = "Id can't be empty")
+    @NotNull(message = "Id can't be empty")
     private Integer id;
 
-    @NotEmpty(message = "Days can't be empty")
+    @NotNull(message = "Days can't be empty")
     @JsonFormat
             (shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     private Date leaveDay;
 
-    @NotEmpty(message = "Days can't be empty")
+    @NotNull(message = "Days can't be empty")
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     @JsonFormat
             (shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date returnDay;
 
-    @NotEmpty(message = "Reason can't be empty")
+    @NotNull(message = "Reason can't be empty")
     private String reason;
 
 
--- internshipdemo/src/main/java/com/kafein/internshipdemo/service/LeaveService.java
+++ internshipdemo/src/main/java/com/kafein/internshipdemo/service/LeaveService.java
@@ -1,11 +1,17 @@
 package com.kafein.internshipdemo.service;
 
+import com.kafein.internshipdemo.entity.Employee;
 import com.kafein.internshipdemo.entity.Leave;
+import com.kafein.internshipdemo.exceptions.DaysCantBeNegativeException;
+import com.kafein.internshipdemo.exceptions.EmployeeNotEnoughDaysException;
+import com.kafein.internshipdemo.exceptions.EmployeeNotFoundException;
 import com.kafein.internshipdemo.repository.EmployeeRepository;
 import com.kafein.internshipdemo.repository.LeaveRepository;
+import com.kafein.internshipdemo.requests.BreakUpdateRequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -15,6 +21,9 @@ public class LeaveService implements ILeaveService{
 
     @Autowired
     private LeaveRepository leaveRepository;
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @Override
     public List<Leave> findAll() {
@@ -38,8 +47,41 @@ public class LeaveService implements ILeaveService{
 
     @Override
     @Transactional
-    public Leave save(Leave theLeave) {
-        return leaveRepository.save(theLeave);
+    public Leave save(BreakUpdateRequestBody body ) {
+
+        Employee employee = employeeService.findById(body.getId());
+
+        Integer newBreakDuration = getBreakDuration(body, employee);
+
+        employee.setNumDaysBreak(newBreakDuration);
+
+        Leave leave = new Leave();
+        leave.setEmployee(employee);
+        leave.setLeaveDay(body.getLeaveDay());
+        leave.setReturnDay(body.getReturnDay());
+        leave.setCreatedAt(System.currentTimeMillis());
+        leave.setReason(body.getReason());
+
+        employeeService.update(employee);
+        return leaveRepository.save(leave);
+    }
+
+    private static Integer getBreakDuration(BreakUpdateRequestBody body, Employee employee) {
+        if (employee == null){
+            throw new EmployeeNotFoundException("Employee id not found: " + body.getId());
+        }
+
+        int days = (int)( (body.getReturnDay().getTime() - body.getLeaveDay().getTime()) / (1000 * 60 * 60 * 24) );
+
+        Integer newBreakDuration = employee.getNumDaysBreak() - days;
+
+        if (days < 0){
+            throw new DaysCantBeNegativeException("Employee can't take negative days off.");
+        }
+        if (newBreakDuration < 0){
+            throw new EmployeeNotEnoughDaysException("Employee doesn't have enough leave rights for " + days + " days.");
+        }
+        return newBreakDuration;
     }
 
     @Override
